30d1779f131079bd11e9c78d6715d76f1a049748
GameDevC <chldustmd234@gmail.com>
2023-05-17 02:31:15 +0900

    BOJ9996 fix

--- Appendix A/BOJ9996.cpp
+++ Appendix A/BOJ9996.cpp
@@ -8,12 +8,11 @@ int main(){
     int n;
     string pattern;
     cin >> n >> pattern;
-    int star = pattern.find('*');
-    string front = pattern.substr(0, star);
-    string rear = pattern.substr(star + 1, pattern.size() - star);
+    int staridx = pattern.find('*');
+    string front = pattern.substr(0, staridx);
+    string rear = pattern.substr(staridx + 1, pattern.size() - staridx);
 
     while(n--){
-        bool check = true;
         string file;
         cin >> file;
 
@@ -22,17 +21,19 @@ int main(){
             continue;
         }
 
-        for(int i = 0; i < star; i++){
-            if(front[i] != file[i]){
-                check = false;
-                break;
-            }
+        // file.find(front)의 결과가 0번째 위치를 반환하지 않으면 패턴과 일치하지 않음
+        if(file.find(front) != 0){
+            cout << "NE" << '\n';
+            continue;
         }
 
-        if((file.find(rear, file.size() - rear.size()) == -1))
-            check = false;
-
-        if(check) cout << "DA" << '\n';
-        else cout << "NE" << '\n';
+        // file.find(rear)를 뒤에서부터 rear의 길이 만큼 뺀 위치부터 
+        // 수행 했을 때 찾지 못하면 패턴과 일치하지 않음
+        if((file.find(rear, file.size() - rear.size()) == -1)){
+            cout << "NE" << '\n';
+            continue;
+        }
+        
+        cout << "DA" << '\n';
     }
 }
