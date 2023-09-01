c59f852a22920650f0bf0146de3b54f9d90b87a2
GameDevC <chldustmd234@gmail.com>
2023-09-01 16:03:15 +0900

    Level 2 : 41 / 106

---
+++ Programmers/Level 2/42584.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<string> split(string s, string sep){
+    vector<string> ret;
+    int pos = 0;
+    while(pos < s.size()){
+        int nxt_pos = s.find(sep, pos);
+        if(nxt_pos == -1) nxt_pos = s.size();
+        if(nxt_pos - pos > 0)
+            ret.push_back(s.substr(pos, nxt_pos - pos));
+        pos = nxt_pos + sep.size();
+    }
+    return ret;
+}
+
+vector<string> solution(vector<string> record) {
+    vector<string> answer;
+    queue<pair<string, string>> q;
+    map<string, string> m;
+
+    for(auto x : record){
+        vector<string> tmp = split(x, " ");
+
+        if(tmp[0] == "Enter"){
+            q.push({tmp[0], tmp[1]});
+            m[tmp[1]] = tmp[2];
+        }
+        else if(tmp[0] == "Leave"){
+            q.push({tmp[0], tmp[1]});
+        }
+        else if(tmp[0] == "Change")
+            m[tmp[1]] = tmp[2];
+    }
+
+    while(!q.empty()){
+        if(q.front().first == "Enter")
+            answer.push_back(m[q.front().second] +"´ÔÀÌ µé¾î¿Ô½À´Ï´Ù.");
+        else
+            answer.push_back(m[q.front().second] +"´ÔÀÌ ³ª°¬½À´Ï´Ù.");
+        q.pop();
+    }
+
+    return answer;
+}
