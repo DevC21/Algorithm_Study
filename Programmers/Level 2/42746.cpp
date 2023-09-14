f5c9b663f65c1f76cc585ce72caebc5b0e0d2699
GameDevC <chldustmd234@gmail.com>
2023-09-14 11:28:23 +0900

    Level 2 : 54 / 106

--- Programmers/Level 2/42746.cpp
+++ Programmers/Level 2/42746.cpp
@@ -1,46 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool isprime(int n){
-    if (n == 1 || n == 0) return 0;
-    for(int i = 2; i*i <= n; i++){
-        if(n % i == 0) 
-            return 0;
-    }
-    return 1;
+bool cmp(string s1, string s2){
+    if(s1 + s2 > s2 + s1)
+        return true;
+    else
+        return false;
 }
 
-char arr[8];
-bool isused[8];
-set<int> prime;
+string solution(vector<int> numbers) {
+    string answer = "";
 
-void func(int n, int size, string s){
-    if(n == size)
-        return;
+    vector<string> v;
+    for(auto x : numbers)
+        v.push_back(to_string(x));
 
-    for(int i = 0; i < size; i++){
-        if(!isused[i]){
-            s += arr[i];
-            isused[i] = true;
-            if(isprime(stoi(s)))
-                prime.insert(stoi(s));
-            func(n + 1, size, s);
-            s.pop_back();
-            isused[i] = false;
-        }
-    }
-}
+    sort(v.begin(), v.end(), cmp);
 
+    if(v[0] == "0")
+        return "0";
 
-int solution(string numbers) {
-    int answer = 0;
-
-    for(int i = 0; i < numbers.size(); i++)
-        arr[i] = numbers[i];
-    
-    func(0, numbers.size(), "");
-
-    answer = prime.size();
+    for(auto x : v)
+        answer += x;
 
     return answer;
 }
