e58ef7c1832dd702292d527c2f244c2600c10640
GameDevC <chldustmd234@gmail.com>
2023-09-21 19:26:47 +0900

    자바 재활 VarEx3

---
+++ JAVA/ch2/VarEx3.java
@@ -0,0 +1,12 @@
+package JAVA.ch2;
+
+public class VarEx3 {
+    
+    public static void main(String[] args){
+        // score에 final을 붙여서 상수가 되므로 값 변경 불가
+        final int score = 100;
+        // score = 200; 
+
+        System.out.println(score);
+    }
+}
