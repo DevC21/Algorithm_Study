a19f0963445941572e16772ae878f00d4d45212c
GameDevC <chldustmd234@gmail.com>
2023-10-16 17:47:23 +0900

    자바재활 64 / 171

--- JAVA/ch6/Ex6_4.java
+++ JAVA/ch6/Ex6_4.java
@@ -9,6 +9,7 @@ public class Ex6_4 {
         long result2 = mm.subtract(5L, 3L);
         long result3 = mm.multiply(5L, 3L);
         double result4 = mm.divide(5L, 3L);
+        mm.print99dan(12);
 
         System.out.println("max(5L, 3L) = "      + result_max);
         System.out.println("min(5L, 3L) = "      + result_min);
@@ -20,8 +21,20 @@ public class Ex6_4 {
 }
 
 class MyMath {
+    void print99dan(int dan) {
+        if(!(2 <= dan && dan <= 9))
+            return; // 입력받은 단(dan)이 2 ~ 9가 아니면, 메서드 종료하고 돌아가기
+
+        for(int i = 1; i <= 9; i++){
+            System.out.printf("%d * %d = %d\n", dan, i, dan * i);
+        }
+
+        return;
+    }
+
+
     long add(long a, long b) {
-        long result = a+b;
+        long result = a + b;
         return result;
     //	return a + b;	// 위의 두 줄을 이와 같이 한 줄로 간단히 할 수 있다.
     }
